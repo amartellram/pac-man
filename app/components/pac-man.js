401c22132a20d57143b80afa397465d0c6d49519
Abel Martell <abel.martell@avantica.net>
2018-04-24 18:10:47 -0500

    added moving circle

--- app/components/pac-man.js
+++ app/components/pac-man.js
@@ -2,15 +2,19 @@ import Component from '@ember/component';
 import KeyboardShortCuts from 'ember-keyboard-shortcuts/mixins/component';
 
 export default Component.extend(KeyboardShortCuts, {
-    didInsertElement: function(){
-        this.drawCircle();
-    },
-    drawCircle: function() {
+    x: 50,
+    y: 100,
+    squareSize: 40,
+    ctx: Ember.computed(function(){
         let canvas = document.getElementById("myCanvas");
         let ctx = canvas.getContext("2d");
-        let x = 50;
-        let y = 100;
-        let radius = 20;
+        return ctx;
+    }),
+    drawCircle: function() {        
+        let ctx = this.get('ctx');
+        let x = this.get('x');
+        let y = this.get('y');
+        let radius = this.get('squareSize')/2;
        
         ctx.fillStyle = '#000';
         ctx.beginPath();
@@ -18,11 +22,31 @@ export default Component.extend(KeyboardShortCuts, {
         ctx.closePath();
         ctx.fill();
     },
+    clearScreen: function(){
+        let ctx = this.get('ctx');
+        let screenWidth = 800;
+        let screenHeight = 600;
+
+        ctx.clearRect(0, 0, screenWidth, screenHeight);
+    },
+    movePacman: function(direction, amount){
+        this.incrementProperty(direction, amount);
+        this.clearScreen();
+        this.drawCircle();
+    },
     keyboardShortcuts: {
-        up: function(){console.log("up");},
-        down: function(){console.log("down");},
-        left: function(){console.log("left");},
-        right: function(){console.log("right");},
+        up: function(){
+            this.movePacman('y', -1*this.get('squareSize'));
+        },
+        down: function(){
+            this.movePacman('y', this.get('squareSize'));
+        },
+        left: function(){
+            this.movePacman('x', -1 * this.get('squareSize'));
+        },
+        right: function(){
+            this.movePacman('x', this.get('squareSize'));
+        },
     },
 });
 
